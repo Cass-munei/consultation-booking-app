7f24143fa00de6d0c6fd7939c047a7afc0dff5d1
Cass-munei <223004579@stud.cut.ac.za>
2026-05-04 12:27:47 +0200

    Update booking_detail.dart

--- booking_detail.dart
+++ booking_detail.dart
@@ -1,14 +1,4 @@
-/*
-N.R Mabunda   223037726
-A.T Rantoa    222027706
-M.C Munyai    223004579
-N.T Ngcobo    222006273
-T.A Mokaleng  223029362
-K Mkhatshwa   222078465
-L.W Rabothata 223062005
 
-Question: booking details.dart
-*/
 
 import 'package:consultationbookingapp/viewmodels/auth_view_model.dart';
 import 'package:consultationbookingapp/viewmodels/booking_view_model.dart';
